4bcec63388ae86af020bf61bafe0ef7143c64150
Aleksey <ekbdizzy@yandex.ru>
2020-01-14 15:57:57 +0300

    added getting dynamic course list from backend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import './static/js/common';
 import './static/js/auth_form';
 import './static/js/list_lessons';
+import './static/js/get_courses_list';
 import './static/css/main.css';
 import './static/scss/main.scss';
 
---
+++ src/static/js/get_courses_list.js
@@ -0,0 +1,52 @@
+'use strict';
+import axios from 'axios'
+
+const HOST = 'http://localhost:8000',
+    COURSES_LIST_URL = `${HOST}/api/course/list`,
+    PAGE_TITLE = 'Список курсов';
+
+document.addEventListener("DOMContentLoaded", () => {
+    function getCoursesList() {
+
+        const title = document.querySelector('title'),
+            courses = document.querySelector('.courses');
+        if (title.innerText === PAGE_TITLE) {
+
+            try {
+                axios.get(COURSES_LIST_URL, {}
+                ).then(response => {
+                    response['data'].forEach(function (item) {
+                        const id = item.id,
+                            title = item.title,
+                            description = item.description,
+                            start_date = item.start_date,
+                            price = item.price,
+                            duration = item.duration,
+                            icon = `${HOST}${item.icon}`;
+
+                        courses.innerHTML += `
+                            <div class="course_item">
+                                <div class="course_item__info">
+                                    <h3 class="title title__course">${title}</h3>
+                                    <p class="course_item__text">${description}</p>
+                                    <a href="" class="main_page__button main_page__button-info">Перейти к курсу</a>
+                                    <div class="course_item__text">
+                                        <span>Старт: ${start_date}</span>
+                                        <span>Продолжительность: ${duration}</span>
+                                    </div>
+                                </div>
+                                <img src="${icon}" alt="${title}">
+                            </div>`
+                    });
+                }).catch((err) => {
+                    console.log("AXIOS: " + err);
+                    courses.innerHTML += `<h1>Ошибка загрузки с сервера, возможно проблема с CORS</h1>`
+                });
+            } catch (err) {
+                console.log("AXIOS: " + err);
+            }
+        }
+    }
+    getCoursesList();
+});
+
